7217ad79799d20880fc6cc3a4a675049ae19d07f
Dhanush <dhanush@pdx.edu>
2016-05-30 16:07:10 -0700

    Add files via upload

--- testLRU.sv
+++ testLRU.sv
@@ -2,8 +2,9 @@ module testLRU();
 
 parameter ASSOCIATIVITY=4;
 parameter WAYREPBITS=($clog2(ASSOCIATIVITY));
-logic [ASSOCIATIVITY:1][WAYREPBITS:0] 	LRUREG;
+logic [ASSOCIATIVITY:1][WAYREPBITS:0] 	LRUREG , LRUREG1;
 logic [WAYREPBITS:0] MRUWay;
+parameter Delay =10ns;
 
 initial
 begin
@@ -14,24 +15,81 @@ LRUREG[1]=3'd2;
 $display("LRU is now %d , %d , %d , %d", LRUREG[4],LRUREG[3],LRUREG[2],LRUREG[1]);
 MRUWay=3;
 UpdateLRU;
-
+#Delay
 MRUWay=1;
 UpdateLRU;
-
+#Delay
 MRUWay=4;
 UpdateLRU;
-
+#Delay
 MRUWay=2;
 UpdateLRU;
-
+#Delay
 LRUREG[4]=3'd3;
 LRUREG[3]=3'd1;
 LRUREG[2]=3'd0;
 LRUREG[1]=3'd0;
-
 MRUWay=4;
+$display("LRU is now %d , %d , %d , %d", LRUREG[4],LRUREG[3],LRUREG[2],LRUREG[1]);
+
+LRUREG[4]=3'd3;
+LRUREG[3]=3'd1;
+LRUREG[2]=3'd4;
+LRUREG[1]=3'd2;
+
+for (int i=0 ; i < 500 ; i++ ) begin
+$display("LRU is now %d , %d , %d , %d", LRUREG[4],LRUREG[3],LRUREG[2],LRUREG[1]);
+LRUREG1 = LRUREG ; 
+MRUWay = $urandom_range(3'd4,3'd1);
+$display("The value of the MRUWay is:%p", MRUWay);
 UpdateLRU;
+UpdateLRUcheck;
+#Delay;
+//$display("The LRU:", LRUREG[4],LRUREG[3],LRUREG[2],LRUREG[1]);
+$display("****************************************************");
 end
+LRUREG[4]=3'd3;LRUREG[3]=3'd0;LRUREG[2]=3'd0;LRUREG[1]=3'd0;
+UpdateLRUcheck;
+#Delay
+LRUREG[4]=3'd3;LRUREG[3]=3'd1;LRUREG[2]=3'd0;LRUREG[1]=3'd0;
+UpdateLRUcheck;
+#Delay
+LRUREG[4]=3'd3;LRUREG[3]=3'd1;LRUREG[2]=3'd2;LRUREG[1]=3'd0;
+UpdateLRUcheck;
+#Delay
+$stop();
+
+end
+
+C1:cover property(A1);
+
+task UpdateLRUcheck;
+
+if (LRUREG1[1] == MRUWay)
+A1: assert(LRUREG[ASSOCIATIVITY:1]=={LRUREG1[1],LRUREG1[ASSOCIATIVITY:2]}) else $error (" Rank1 error");
+if (LRUREG1[2] == MRUWay)
+assert(LRUREG[ASSOCIATIVITY:2]=={LRUREG1[2],LRUREG1[ASSOCIATIVITY:3]})  else $error ("Rank2 error");
+if (LRUREG1[3] == MRUWay)
+assert(LRUREG[ASSOCIATIVITY:3]=={LRUREG1[3],LRUREG1[ASSOCIATIVITY:4]})  else $error ("Rank3 error");
+if (LRUREG1[4] == MRUWay)
+assert(LRUREG[ASSOCIATIVITY:1]==LRUREG1[ASSOCIATIVITY:1])  else $error ("Rank4 error");
+if (~LRUREG[4] || ~LRUREG[3] || ~LRUREG[2] || ~LRUREG[1])
+
+begin
+if (LRUREG[4] == 1'b0) 
+assert (LRUREG[3] == 0 && LRUREG[2] ==1'b0 && LRUREG[1] == 0) else $error (" error in LRUREG[3] or LRUREG[2] or LRUREG[1]");
+
+else if (LRUREG[4] != 1'b0 && LRUREG[3] == 1'b0 ) 
+assert (LRUREG[2] ==1'b0 && LRUREG[1] == 0) else $error ("error in LRUREG[2] or LRUREG[1]");
+
+else if (LRUREG[4] != 1'b0 && LRUREG[3] != 1'b0 && LRUREG[2] == 1'b0 ) 
+assert (LRUREG[1] == 0) else $error ("error in  LRUREG[1]");
+
+end
+
+
+
+endtask
 
 
 
